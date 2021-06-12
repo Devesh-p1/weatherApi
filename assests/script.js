4c8ad4a4895da009799b12576d5a25103533f9f6
unknown <deveshpatel1@outlook.com>
2021-06-11 21:55:23 -0400

    added api calls and div creations in js

--- assests/script.js
+++ assests/script.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   function Weather(input) {
     $ajax({
       type: 'GET',
-      url: "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=0337bcdaa7cdc4b88a62007ca6525db4&units=metric",
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=&units=metric",
       dataType: 'json',
       success: function(data) {
         if (prevCity.indexOf(input) === -1) {
@@ -50,7 +50,32 @@ $(document).ready(function() {
   }
 
   function Forecast(input) {
-    $.ajax
+    $.ajax({
+      type: 'GET',
+      url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=&units=metric",
+      dataType: 'json',
+      success: function(data) {
+        $('#forcast').html('<h4>5-day Forcast:<h4>').append('<div>')
+
+        for (var i = 0; i < data.list.length; i++) {
+
+          if (data.list[i].dt_txt.indexOf('15:00:00') !== -1){
+            let column = $('<div>').addClass('col-md-2');
+            let div = $('<div>').addClass('Ftext')
+            let Dbody = $('<div>').addClass('Fbody')
+
+            let name = $('<h5>').addClass('title').text(new Date(data.list[i].dt_txt).toLocaleDateString());
+            let icon = $('<img>').attr('src', "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
+
+            let para1 = $('<p>').addClass('div-content').text('Temp: ' + data.list[i].main.temp_max + ' C')
+            let para2 = $('<p>').addClass('div-content').text('Humidity: ' + data.list[i].main.humidity + '%')
+
+            col.append(div.append(Dbody.append(name, icon, para1, para2)));
+            $('#forcast').append(col);
+          }
+        }
+      }
+    })
   }
 });
 
